92b31bcb70b8b11a3f3fb93c612888e8dc022700
IqraMomin <iqra2018momin@gmail.com>
2025-11-11 03:49:40 +0530

    Notification 2

--- src/App.js
+++ src/App.js
@@ -2,24 +2,35 @@ import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { uiActions } from './store/uiSlice';
 
 function App() {
   const showCart = useSelector(store => store.ui.cartIsVisible);
   const cart = useSelector(state => state.cart);
+  const dispatch = useDispatch();
+  const notification = useSelector(state=>state.ui.notification);
 
   useEffect(() => {
     const sendCartData = async () => {
       try {
-        fetch("https://advance-redux-8144d-default-rtdb.firebaseio.com//cart.json", {
+        const response = await fetch("https://advance-redux-8144d-default-rtdb.firebaseio.com//cart.json", {
           method: "PUT",
           body: JSON.stringify(cart)
         })
+        if(!response.ok){
+          dispatch(uiActions.showNotification({
+            status:"pending",
+            title:"Error!",
+            message:"Sending cart Data failed!"
+          }))
+        }
       } catch (err) {
 
       }
 
     }
+    sendCartData();
 
   }, [cart])
   return (
--- src/store/uiSlice.js
+++ src/store/uiSlice.js
@@ -11,6 +11,13 @@ const uiSlice = createSlice({
     reducers:{
         toggle:(state)=>{
             state.cartIsVisible = !state.cartIsVisible
+        },
+        showNotification:(state,action)=>{
+            state.notification = {
+                status:action.payload.status,
+                title:action.payload.title,
+                message:action.payload.message
+            }
         }
         
     }
